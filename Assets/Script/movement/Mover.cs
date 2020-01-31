27f40ea5e4e221125f69fc35bbb9fe62939e0077
david <crazydavid875@gmail.com>
2020-02-01 01:30:51 +0800

    Left&&Right
    Left&&Right

---
+++ Assets/Script/movement/Mover.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+namespace GGJ.Movement{
+    public class Mover : MonoBehaviour {
+        [SerializeField] private float speed;
+        public void ForceToMove(Vector3 vector){
+            GetComponent<Rigidbody>().AddForce(vector*speed);
+        }
+    }
+}
---
+++ Assets/Script/player/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GGJ.Movement;
+namespace GGJ.Control{
+    public class PlayerController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            InteractWithMovment();
+        }
+        private void InteractWithMovment(){
+            GetComponent<Mover>().ForceToMove(Sinput.GetVector("Horizontal","",""));
+        }
+    }
+}
