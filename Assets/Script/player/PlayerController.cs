42c78aa1a18887b639c0f8f3da11257290bf1f9d
AaronXue <her8153101@yahoo.com.tw>
2020-02-01 17:58:04 +0800

    Setup animator
    1. Idel
    2. Walk
    3. Pick

--- Assets/Script/player/PlayerController.cs
+++ Assets/Script/player/PlayerController.cs
@@ -8,33 +8,61 @@ namespace GGJ.Control{
     {
         [SerializeField]private GameObject tool;
         [SerializeField]private GameObject pro;
-        [SerializeField]private Vector3 vectical;
+        Animator animator;
+        bool isPicking;
         // Start is called before the first frame update
         void Start()
         {
-            
+            animator = GetComponent<Animator>();
+            isPicking = false;
         }
 
         // Update is called once per frame
         void Update()
         {
-            vectical  = Sinput.GetVector("", "Vertical", "");
             InteractWithMovment();
             InteractWithElevator();
             InteractWithRepare();
+            InteractWIthPick();
         }
         private void InteractWithMovment(){
+            if (isPicking) return;
             GetComponent<Mover>().ForceToMove(Sinput.GetVector("Horizontal", "", ""));
+            if (Sinput.GetVector("Horizontal", "", "").x < 0) transform.localScale = new Vector3(1f, 1f, -1f);
+            if (Sinput.GetVector("Horizontal", "", "").x > 0) transform.localScale = new Vector3(1f, 1f, 1f);
+            transform.rotation = Quaternion.Euler(0, 95, 0);
+            animator.SetFloat("Run", Mathf.Abs(Sinput.GetVector("Horizontal", "", "").x));
         }
         private void InteractWithElevator()
         {
-            GetComponent<Elevator>().Boost(Sinput.GetVector("", "Vertical", ""));
+            if (Sinput.GetButtonDown("Up"))
+            {
+                GetComponent<Elevator>().Boost(Sinput.GetVector("", "Vertical", ""));
+            }
+            else if (Sinput.GetButtonDown("Down"))
+            {
+                GetComponent<Elevator>().Boost(Sinput.GetVector("", "Vertical", ""));
+            }
         }
         private void InteractWithRepare(){
             if(Sinput.GetButtonDown("Use")){
                 GetComponent<User>().UseTool(tool.transform,pro.transform);
-   
             }
         }
+        private void InteractWIthPick()
+        {
+            if (Sinput.GetButtonDown("Submit") && !isPicking)
+            {
+                isPicking = true;
+                animator.SetTrigger("Pick");
+                StartCoroutine(Picking());
+            }
+        }
+        IEnumerator Picking()
+        {
+            yield return new WaitForSeconds(1.1f);
+            isPicking = false;
+        }
+
     }
 }
