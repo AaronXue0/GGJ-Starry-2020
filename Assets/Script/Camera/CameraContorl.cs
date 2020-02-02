ef42ac23601c3bc956cf235d9ec960d9bf142e5b
AaronXue <her8153101@yahoo.com.tw>
2020-02-02 10:48:58 +0800

    Setting up camera

--- Assets/Script/Camera/CameraContorl.cs
+++ Assets/Script/Camera/CameraContorl.cs
@@ -6,6 +6,9 @@ public class CameraContorl : MonoBehaviour
 {
     GameObject player;
 
+    float[] yPosition = { 0, 2.4f, 6 };
+    float cameraY;
+
     private void Start()
     {
         player = GameObject.Find("Player");
@@ -13,6 +16,9 @@ public class CameraContorl : MonoBehaviour
 
     private void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        if(player.transform.position.y >= yPosition[1]) cameraY = yPosition[1];
+        else if (player.transform.position.y >= yPosition[2]) cameraY = yPosition[2];
+        else cameraY = yPosition[0];
+        transform.position = new Vector3(player.transform.position.x, cameraY, transform.position.z);
     }
 }
