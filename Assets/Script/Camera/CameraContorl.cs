e9891f09f21821da9ec5dab761dee89c2917f8a7
david <crazydavid875@gmail.com>
2020-02-02 14:25:52 +0800

    scene-arrange

--- Assets/Script/Camera/CameraContorl.cs
+++ Assets/Script/Camera/CameraContorl.cs
@@ -12,6 +12,6 @@ public class CameraContorl : MonoBehaviour
 
     private void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y+1, transform.position.z);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y+0.5f, transform.position.z);
     }
 }
